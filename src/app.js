8cf5933e90a77ac4c6959fb5d51d88b78b6f6dde
FuuNuka <75213697+FuuNuka@users.noreply.github.com>
2022-08-25 11:17:54 +0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 
-const app = express()
+const app = exprpess()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extened: true}))
 
 require('./route.js')(app)
-
-
-let port = 8081
+//ข้อ1
+app.put('/app',function add(input,input){
+    return input+input;
+    }
+)
+//2
+app.put('/app',function add(input){
+    sum =1;
+    for (input =1; input<=input;input++){
+        sum *= input;
+    
+    }
+    console.log("%input! = %input",sum)
+    retrun =0   ;
+}
+)
+let port = 8085
 
 app.listen(port, function () {
     console.log('server running on ' + port)
